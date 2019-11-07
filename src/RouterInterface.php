16227c5ca96c49b75e7b9a6b5a53ab141a93ae7d
Murat Bastuerk <murat@basturk.de>
2019-11-07 11:14:52 +0100

    add docblocks and implement missing methods

--- src/RouterInterface.php
+++ src/RouterInterface.php
@@ -4,27 +4,62 @@
 namespace Mb7\EzPhp\Router;
 
 
-use Mb7\EzPhp\Mvc\Controller\AbstractController;
-
+/**
+ * Interface RouterInterface
+ * @package Mb7\EzPhp\Router
+ */
 interface RouterInterface
 {
+    /**
+     * @return string
+     */
     public function getHost(): string;
 
+    /**
+     * @return string
+     */
     public function getPort(): string;
 
+    /**
+     * @return string
+     */
     public function getURl(): string;
 
+    /**
+     * @return array
+     */
     public function getUrlRouteParams(): array;
 
+    /**
+     * @return array
+     */
     public function getParams(): array;
 
+    /**
+     * @return string
+     */
     public function getProtocol(): string;
 
+    /**
+     * @return array
+     */
     public function getPostParams(): array;
 
+    /**
+     * @param string $defaultRoute
+     * @return mixed
+     */
     public function setDefaultRoute(string $defaultRoute);
 
+    /**
+     * @param string $relativeUrl
+     * @return mixed
+     */
     public function routeToRelativeURL(string $relativeUrl);
 
+    /**
+     * @param string $url
+     * @return mixed
+     */
     public function routeToURL(string $url);
 }
--- src/SimpleStaticRouter.php
+++ src/SimpleStaticRouter.php
@@ -3,13 +3,35 @@
 
 namespace Mb7\EzPhp\Router;
 
+/**
+ * Class SimpleStaticRouter
+ * @package Mb7\EzPhp\Router
+ */
 class SimpleStaticRouter implements RouterInterface
 {
+    /**
+     * @var
+     */
     private $server;
+    /**
+     * @var
+     */
     private $get;
+    /**
+     * @var
+     */
     private $post;
+    /**
+     * @var string
+     */
     private $defaultRoute = "/index/index";
 
+    /**
+     * SimpleStaticRouter constructor.
+     * @param $server
+     * @param $get
+     * @param $post
+     */
     public function __construct($server, $get, $post)
     {
         $this->server = $server;
@@ -17,21 +39,33 @@ class SimpleStaticRouter implements RouterInterface
         $this->post = $post;
     }
 
+    /**
+     * @inheritDoc
+     */
     public function getHost(): string
     {
         return $this->server["HTTP_HOST"];
     }
 
+    /**
+     * @inheritDoc
+     */
     public function getPort(): string
     {
         return $this->server["SERVER_PORT"];
     }
 
+    /**
+     * @inheritDoc
+     */
     public function getProtocol(): string
     {
         return $this->server["SERVER_PROTOCOL"];
     }
 
+    /**
+     * @inheritDoc
+     */
     public function getUrlRouteParams(): array
     {
         if (count($this->getUrlParts($this->getURl())) > 3) {
@@ -40,16 +74,25 @@ class SimpleStaticRouter implements RouterInterface
         return array();
     }
 
+    /**
+     * @inheritDoc
+     */
     public function getParams(): array
     {
         return $this->get;
     }
 
+    /**
+     * @inheritDoc
+     */
     public function getPostParams(): array
     {
         return $this->post;
     }
 
+    /**
+     * @inheritDoc
+     */
     public function getURl(): string
     {
         if ($this->checkValueInArray("REDIRECT_URL", $this->server)) {
@@ -57,22 +100,35 @@ class SimpleStaticRouter implements RouterInterface
         } else return $this->defaultRoute;
     }
 
+    /**
+     * @inheritDoc
+     */
     public function setDefaultRoute(string $defaultRoute)
     {
         $this->defaultRoute = $defaultRoute;
     }
 
+    /**
+     * @inheritDoc
+     */
     public function routeToURL(string $url)
     {
-        // TODO: Implement routeToURL() method.
+        header("Location: $url");
     }
 
+    /**
+     * @inheritDoc
+     */
     public function routeToRelativeURL(string $relativeUrl)
     {
-        // TODO: Implement routeToRelativeURL() method.
+        $route = $this->getHost().$relativeUrl;
+        header("Location: $route");
     }
 
-
+    /**
+     * @param $url
+     * @return array
+     */
     protected function getUrlParts($url)
     {
         return explode("/", $url);
